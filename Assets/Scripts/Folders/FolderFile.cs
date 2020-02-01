719b3af63b53e0e8abce3fb5a81e761d5971fbc7
Matthew <MatthewFerreira514@gmail.com>
2020-02-01 17:18:16 -0500

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Folders/FolderFile.cs
+++ Assets/Scripts/Folders/FolderFile.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FolderFile", menuName = "FolderFile/NewFolder", order = 2)]
 public abstract class FolderFile : ScriptableObject
 {
+    public FolderFile[] folders;
+    
+    [Space(10)]
     public FileData[] files;
     public virtual void InitializeFolder()
     {
